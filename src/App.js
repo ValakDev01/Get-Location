e0d92e37a63bc54d8ec1be64410f9ca2ec8fa03a
Dominik Walczak <valakdev01@dominiks-mbp.home>
2024-09-07 12:28:14 +0200

    Get Location App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function useGeolocation() {
+	const [isLoading, setIsLoading] = useState(false);
+	const [position, setPosition] = useState({});
+	const [error, setError] = useState(null);
+
+	function getPosition() {
+		if (!navigator.geolocation)
+			return setError("Your browser does not support geolocation");
+
+		setIsLoading(true);
+		navigator.geolocation.getCurrentPosition(
+			(pos) => {
+				setPosition({
+					lat: pos.coords.latitude,
+					lng: pos.coords.longitude,
+				});
+				setIsLoading(false);
+			},
+			(error) => {
+				setError(error.message);
+				setIsLoading(false);
+			}
+		);
+	}
+
+	return { isLoading, position, error, getPosition };
 }
 
-export default App;
+export default function App() {
+	const {
+		isLoading,
+		position: { lat, lng },
+		error,
+		getPosition,
+	} = useGeolocation();
+
+	const [countClicks, setCountClicks] = useState(0);
+
+	function handleClick() {
+		setCountClicks((count) => count + 1);
+		getPosition();
+	}
+
+	return (
+		<div>
+			<button onClick={handleClick} disabled={isLoading}>
+				Get my position
+			</button>
+
+			{isLoading && <p>Loading position...</p>}
+			{error && <p>{error}</p>}
+			{!isLoading && !error && lat && lng && (
+				<p>
+					Your GPS position:{" "}
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+					>
+						{lat}, {lng}
+					</a>
+				</p>
+			)}
+
+			<p>You requested position {countClicks} times</p>
+		</div>
+	);
+}
